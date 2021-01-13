982fbf578e925630f0318c4c10491f5a4190531c
eliashiv903 <58138902+eliashiv903@users.noreply.github.com>
2021-01-13 16:12:22 +0200

    update Robot for gui

--- Robot.py
+++ Robot.py
@@ -1,15 +1,15 @@
 from Point import Point
 
 class Robot:
-      def __init__(self, current_place:Point, end_place:Point):
+      def __init__(self, current_place:Point, end_place:Point, num):
             self.current_place = current_place
             self.end_place = end_place
             self.stuck = 0
+            self.num = num
+
 
       def __str__(self):
             return "Current Place: " + str(self.current_place) + "End Place: " + str(self.end_place)
 
       def __repr__(self):
             return "Robot"
-            # return "Start Place: " + str(self.startPlace) + "     Current Place: " + str(self.currentPlace) + "     End Place: " + str(self.endPlace) + "\n"
-

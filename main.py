16a1d05171ee4d35494791da906d374a6016f012
amit1021 <amitelbaz1021@gamil.com>
2021-05-15 22:41:50 +0300

    # Fix robots that stuck

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ def init_game():
 
 
 def start_game(board):
+    # To check if there is stuck robots.
+    stuck = 0
+    number_robots = len(robot_list)
     # While there are robots that have not reached their destination
     while len(robot_list) > 0:
         # Go over the robots that did not reach the destination
@@ -75,6 +78,21 @@ def start_game(board):
                 robot_queue_node = bfs_few_steps(board, robot.current_place, robot.end_place)
                 move_robot_few_steps(board, robot, robot_queue_node)
 
+            if len(robot_list) == 12 or len(robot_list) == 9 or len(robot_list) == 5:
+                stuck = stuck + 1
+            else:
+                stuck = 0
+            number_robots = len(robot_list)
+            if stuck > 4:
+                robot_stuck(board, robot)
+                robot_queue_node = bfs(board, robot.current_place, robot.end_place)
+                if robot_queue_node != -1:
+                    move_robot_all_path(board, robot, robot_queue_node)
+                else:
+                    # Move the robot a few steps the he can
+                    robot_queue_node = bfs_few_steps(board, robot.current_place, robot.end_place)
+                    move_robot_few_steps(board, robot, robot_queue_node)
+
         print("Number of robots that reach their destination:  ", len(robot_in_destination))
         print("Number of robots that are left:  ", len(robot_list))
         print(
@@ -82,10 +100,8 @@ def start_game(board):
         for i in range(len(board)):
             print( i , board[i])
 
-        if len(robot_list) == 12:
+        if len(robot_list) <= 12:
             print(robot_list)
-
-
 
 
 
@@ -132,6 +148,41 @@ def move_robot_few_steps(board,robot, robot_queue_node):
     # Just for checking the function (delete after)
     if not Point.equal(robot.current_place, robot.end_place):
         print("not reach the goal")
+
+
+def robot_stuck(board, robot):
+    n = len(board[0])
+    x = robot.current_place.x
+    y = robot.current_place.y - 1
+    z = 5
+    if n/2 > robot.current_place.y:
+        while y > 0:
+            if board[x][y] != -1 and  board[x][y] != 0:
+                for robot in robot_in_destination:
+                    if robot.robot_number == board[x][y]:
+                        move_robot_out_of_board(board,robot,x - z, y)
+                        break
+            y = y -1
+    else:
+        while y < n:
+            if board[x][y] != -1 and board[x][y] != 0:
+                for robot in robot_in_destination:
+                    if robot.robot_number == board[x][y]:
+                        move_robot_out_of_board(board,robot, x - z, y)
+            y = y + 1
+
+
+def move_robot_out_of_board(board, robot, i, j):
+    p = Point(i, j)
+    robot_queue_node = bfs(board, robot.current_place, p)
+    if robot_queue_node != -1:
+        move_robot_all_path(board, robot, robot_queue_node)
+    else:
+        # Move the robot a few steps the he can
+        robot_queue_node = bfs_few_steps(board, robot.current_place, robot.end_place)
+        move_robot_few_steps(board, robot, robot_queue_node)
+
+
 
 
 def main():
